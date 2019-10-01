ccbcaa1bbee3721ac4430945b028e32f4405bcfa
Ibrahim Shaltout <Ibrahim.shaltout1996@gmail.com>
2019-10-01 17:22:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/activity/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.movieapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.movieapp.R;
+import com.example.movieapp.model.Movie;
+import com.example.movieapp.model.MoviesResponse;
+
+import com.example.movieapp.rest.ApiClient;
+import com.example.movieapp.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // TODO - insert your themoviedb.org API KEY here
+    private final static String API_KEY = "c35a6fa437ce29c28d73f2adee149635";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse>call, Response<MoviesResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received: " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse>call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+    }
+
